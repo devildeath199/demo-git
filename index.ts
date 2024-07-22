5c29d021ad75950bbbc012ee5a28fafe0cb11881
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-07-22 21:15:51 +0530

    branch-commit-one

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 let x = 1;
 console.log(x);
 
-console.log("testing and creating new branch");
-console.log("testing and creating new branch");
+console.log("testing and creating new branch - 1");
+console.log("testing and creating new branch - 1");
+
+
+console.log("testing and creating new branch - 2");
